99d6ecde8c2f00eaa1d0a4a9e33c66f34adccd78
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2025-06-16 21:02:23 +0900

    first commit

---
+++ src/main/java/kr/co/msa/android_be/AndroidBeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.msa.android_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/msa/android_be/AndroidBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.msa.android_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
